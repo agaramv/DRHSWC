f6cd24fd157af786d4b802a9659ada9c4a800d0c
TomasCastilloOK <48834172+TomasCastilloOK@users.noreply.github.com>
2019-04-11 13:33:44 -0400

    form

--- DRHSWS_UI/src/app/signup/form/form.component.ts
+++ DRHSWS_UI/src/app/signup/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  @ViewChild('f') signupForm: NgForm;
+  submitted: boolean = false;
+  grades = ["9th","10th","11th","12th"];
+  data = {
+    firstname: "bob",
+    lastname: '',
+    // grade: '',
+    topic: ''
+  };
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmit() {
+    console.log(this.signupForm)
+    this.data.firstname = this.signupForm.value.firstname;
+    this.data.lastname = this.signupForm.value.lastname;
+    // this.data.grade = this.signupForm.value.grade;
+    this.data.topic = this.signupForm.value.topic;
+    this.submitted = true;
+  }
 }
