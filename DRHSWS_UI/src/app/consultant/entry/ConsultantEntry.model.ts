f1f25c452f950f5b1647fba5ed42c2d952f004d6
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-10 20:44:59 -0400

    ConsultantEntry object

--- DRHSWS_UI/src/app/consultant/entry/ConsultantEntry.model.ts
+++ DRHSWS_UI/src/app/consultant/entry/ConsultantEntry.model.ts
@@ -1,11 +1,21 @@
 export class ConsultantEntry{
-    firstName: '';
-    lastName: '';
-    firstNameS: '';
-    lastNameS: '';
-    emailC: '';
-    teacher: '';
-    feedback1: '';
-    feedback2: '';
-    
+    firstName: string;
+    lastName: string;
+    firstNameS: string;
+    lastNameS: string;
+    emailC: string;
+    teacher: string;
+    feedback1: string;
+    feedback2: string;
+
+    constructor(firstName: string, lastName: string, firstNameS: string, lastNameS: string, emailC: string, teacher: string, feedback1: string, feedback2: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.firstNameS = firstNameS;
+        this.lastNameS = lastNameS;
+        this.emailC = emailC;
+        this.teacher = teacher;
+        this.feedback1 = feedback1;
+        this.feedback2 = feedback2;
+    }
 }
