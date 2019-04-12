f0ee8119ba50933d0a73f95f074fd864cdffd445
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-12 11:36:29 -0400

    admin stuff

--- DRHSWS_UI/DRHSWS_UI/src/app/admin/manage-consultants/manage-consultants.component.ts
+++ DRHSWS_UI/DRHSWS_UI/src/app/admin/manage-consultants/manage-consultants.component.ts
@@ -3,13 +3,8 @@ import { FormControl, Validators } from '@angular/forms';
 import { Consultant } from 'src/app/consultant/consultant.model';
 import { ConsultantService } from 'src/app/consultant/consultant.service';
 import { ConsultantEntry } from 'src/app/consultant/entry/ConsultantEntry.model';
-
-const consultants: Consultant[] = [
-  {first: 'Vidur', last: 'Agaram', email:'stuff@gmail.com'},
-  {first: 'Thomas', last: 'Castillo', email:'stuff@gmail.com'},
-  {first: 'Mike', last: 'Krause', email:'stuff@gmail.com'},
-  {first: 'Naomi', last: 'Nickels', email:'stuff@gmail.com'},
-]
+import { Appointment } from 'src/app/appointment.model';
+import { SignupService } from 'src/app/signup/signup.service';
 
 @Component({
   selector: 'app-manage-consultants',
@@ -19,8 +14,10 @@ const consultants: Consultant[] = [
 export class ManageConsultantsComponent implements OnInit {
   displayedColumnsC: string[] = ['Action', 'Name', 'Email'];
   displayedColumnsR: string[] = ['Action', 'Name', 'Email','Student','Teacher', 'Review'];
+  displayedColumnsA: string[] = ['Date', 'Lunch', 'Student', 'Grade','Teacher', 'Topic'];
   consultants: Consultant[];
   reviews: ConsultantEntry[];
+  appointments: Appointment[];
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
@@ -28,12 +25,13 @@ export class ManageConsultantsComponent implements OnInit {
     return this.email.hasError('required') ? 'You must enter a value' :
         this.email.hasError('email') ? 'Not a valid email' : '';
   }
-  constructor(private consultantService: ConsultantService) { }
+  constructor(private consultantService: ConsultantService, private signupService: SignupService) { }
 
   ngOnInit() {
     console.log(this.consultantService.getAllConsultants());
     this.consultants = this.consultantService.getAllConsultants();
     this.reviews = this.consultantService.getAllReviews();
+    this.appointments = this.signupService.getAllAppointments();
   }
 
 
