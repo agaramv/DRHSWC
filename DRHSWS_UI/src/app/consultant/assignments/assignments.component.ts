260fdc1bb87cafc372a081bdc07e7ecca86f9b2e
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-11 20:10:38 -0400

    admin

--- DRHSWS_UI/src/app/consultant/assignments/assignments.component.ts
+++ DRHSWS_UI/src/app/consultant/assignments/assignments.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Consultant{
+  first: string;
+  last: string;
+  email: string;
+  file: string;
+}
+
+const consultants: Consultant[] = [
+  {first: 'Vidur', last: 'Agaram', email:'stuff@gmail.com', file:'stuff'},
+  {first: 'Thomas', last: 'Castillo', email:'stuff@gmail.com', file: 'buff'},
+  {first: 'Mike', last: 'Krause', email:'stuff@gmail.com', file:'huff'},
+  {first: 'Naomi', last: 'Nickels', email:'stuff@gmail.com', file:'puff'},
+]
+
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.scss']
 })
 export class AssignmentsComponent implements OnInit {
+
+  displayedColumns: string[] = ['name', 'email', 'file'];
+  data: Consultant[] = consultants;
 
   constructor() { }
 
---
+++ DRHSWS_UI/src/app/consultant/consultant.model.ts
@@ -0,0 +1,11 @@
+export class Consultant{
+    first: string;
+    last: string;
+    email: string;
+
+    constructor(first: string, last: string, email: string){
+        this.first = first;
+        this.last = last;
+        this.email = email;
+    }
+}
