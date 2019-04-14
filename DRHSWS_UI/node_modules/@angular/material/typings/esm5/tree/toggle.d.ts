426e5df7d99441514408650e94559fe9c9d28415
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-14 17:56:40 -0400

    fix

--- DRHSWS_UI/node_modules/@angular/material/typings/esm5/tree/toggle.d.ts
+++
@@ -1,8 +0,0 @@
-import { CdkTreeNodeToggle, CdkTree, CdkTreeNode } from '@angular/cdk/tree';
-/**
- * Wrapper for the CdkTree's toggle with Material design styles.
- */
-export declare class MatTreeNodeToggle<T> extends CdkTreeNodeToggle<T> {
-    recursive: boolean;
-    constructor(_tree: CdkTree<T>, _treeNode: CdkTreeNode<T>);
-}
