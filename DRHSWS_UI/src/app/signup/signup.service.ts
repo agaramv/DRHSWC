6d0ead91bf4dac6f8805b866ce24c169eb1946a8
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-14 17:43:02 -0400

    fix

--- DRHSWS_UI/src/app/signup/signup.service.ts
+++ DRHSWS_UI/src/app/signup/signup.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Appointment } from '../appointment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
+  
+  appointments: Appointment[] = [
+    {date: '4/12/19', type: 'A', slot: '2', firstName: 'Vidur', lastName: 'Agaram', grade: 12, teacher:'Berry', topic:'English'},
+    {date: '4/12/19', type: 'B', slot: '3', firstName: 'Tomas', lastName: 'Castillo', grade: 12, teacher:'Berry', topic:'English'},
+    {date: '4/12/19', type: 'B', slot: '4', firstName: 'Mike', lastName: 'Krause', grade: 12, teacher:'Berry', topic:'English'},
+  ]
 
   constructor() { }
 
+  getAllAppointments(){
+    return this.appointments.slice();
+  }
   
 }
