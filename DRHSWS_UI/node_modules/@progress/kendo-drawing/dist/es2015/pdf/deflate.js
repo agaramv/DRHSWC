a4bd9f0ab92a8d4fce615fddffb83fb07f5c4b11
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-14 18:15:56 -0400

    fix

--- DRHSWS_UI/node_modules/@progress/kendo-drawing/dist/es2015/pdf/deflate.js
+++
@@ -1,8 +0,0 @@
-import { deflate as pakoDeflate } from 'pako/dist/pako_deflate';
-
-export const deflate = pakoDeflate;
-
-export function supportsDeflate() {
-    return true;
-}
-
--- DRHSWS_UI/node_modules/angular-pdf/src/angular-pdf.module.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import { NgPdf } from './angular-pdf.directive'
-
-export const Pdf = angular
-  .module('pdf', [])
-  .directive('ngPdf', NgPdf)
-  .name;
-
-export default Pdf;
