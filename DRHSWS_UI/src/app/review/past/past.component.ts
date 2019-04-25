8b034d1ce1553de5d76b0eb089c650eacb4a3ef9
agaramv <42156415+agaramv@users.noreply.github.com>
2019-04-24 22:31:36 -0400

    pastReview

--- DRHSWS_UI/src/app/review/past/past.component.ts
+++ DRHSWS_UI/src/app/review/past/past.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Appointment } from 'src/app/signup/appointment.model';
+import { AppointmentsService } from 'src/app/appointments.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-past',
@@ -9,10 +11,37 @@ import { Appointment } from 'src/app/signup/appointment.model';
 export class PastComponent implements OnInit {
   reviewsPast: Appointment[];
   displayedColumns = ['Date', 'Lunch', 'Student', 'Grade', 'Teacher', 'Review'];
-
-  constructor() { }
-
-  ngOnInit() {
+  filter = {
+    from: '',
+    to: ''
   }
 
+  constructor(private apptService: AppointmentsService) { }
+
+  ngOnInit() {
+    this.getPastAppointments();
+  }
+
+  onSubmit(form: NgForm){
+    this.filter.from = form.value.from;
+    this.filter.to = form.value.to;
+    this.getAppointmentDateRange();
+  }
+
+  getAppointmentDateRange(){
+    this.apptService.getAppointmentsByDateRange(this.filter.from, this.filter.to)
+      .subscribe((data: Appointment[])=>{
+        this.reviewsPast = data;
+      })
+  }
+
+  //get past two weeks appointments
+  getPastAppointments(){
+    this.apptService.getPastAppoinments()
+      .subscribe((data: Appointment[])=>{
+        //console.log(data.appointmentEntityKey);
+        this.reviewsPast = data;
+        
+      })
+  }
 }
